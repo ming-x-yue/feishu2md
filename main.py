4e5c5bc74221ab20b38dd8b1cf851b4e83c5591c
岳明瑄 <yuemingxuan@tezign.com>
2024-06-25 11:52:20 +0800

    Added to my project

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+def validation_exception_handler(request, err):
+    base_error_message = f"Failed to execute: {request.method}: {request.url}"
+    return JSONResponse(status_code=400, content={"message": f"{base_error_message}. Detail: {err}"})
+
